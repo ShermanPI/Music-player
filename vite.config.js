28f945510dc6234f4bc852035057959d7ba1eff5
ShermanPI <shermanperez0@gmail.com>
2023-08-14 20:14:29 -0400

    vite config file fixed

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()]
 })
-
-module.exports = {
-  assets: [
-    path.resolve(__dirname, '../static/img/bohemian.jpg'),
-    path.resolve(__dirname, '../static/img/renegade.jpg'),
-    path.resolve(__dirname, '../static/img/BLANK.jpg'),
-    path.resolve(__dirname, '../static/img/runandhide.jpg')
-
-  ]
-}
